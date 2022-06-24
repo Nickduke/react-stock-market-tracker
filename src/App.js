d4e84f144a421076df942cbe164632e11b7ae65a
nickNtoukas <ntoukas.nick@gmail.com>
2022-06-24 12:57:29 -0400

    Getting data back from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import StockRow from './components/StockRow.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <StockRow ticker='aapl' />
+            <StockRow ticker='goog' />
+            <StockRow ticker='msft' />
+            <StockRow ticker='tsla' />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
